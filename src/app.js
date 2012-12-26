1377affdcfce9902d905440e4150cabe36df4790
Ian Coleman <coleman.ian@gmail.com>
2012-12-27 10:58:29 +1100

    Initial watcher / compiler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,37 @@
+var settings = {
+	new_feed_is_active: true,
+	jsonp_timeout_wait_period: 3000,
+	subreddit_keyup_delay: 1000,
+	auth_token: "",
+	is_logged_in: false,
+	
+	last_visited_subreddit: function(val) {
+		var last_visited_subreddit = get_set("last_visited_subreddit", val)
+		if (typeof(last_visited_subreddit) == undefined) {
+			last_visited_subreddit = "all";
+		}
+		return last_visited_subreddit;
+	},
+	user: function(val) {
+		return get_set("user", val);
+	},
+	modhash: function(val) {
+		return get_set("modhash", val);
+	}
+}
 
+get_set = function(key, val) {
+	if (typeof(val) == "undefined") {
+		return localStorage[key]
+	}
+	else {
+		localStorage[key] = val;
+	}
+}
 
-var Reddit = function() {
+// Displays the data from reddit in the list and iframe,
+// and handles the selection of new subreddits.
+var Reddit_UI = function() {
 
 	var $subreddit_input = $("#subreddit_input");
 	var $reddit_list = $("#reddit_list");
@@ -13,29 +44,14 @@ var Reddit = function() {
 	var list_template = _.template($("#list_template").html());
 	var footer_template = _.template($("#footer_template").html());
 
-	var settings = {
-		new_feed_is_active: true,
-		last_visited_subreddit: "all",
-		jsonp_timeout_wait_period: 3000,
-		subreddit_keyup_delay: 1000
-	}
-
-	$.ajaxSetup({
-		error: function(xhr, text, error) {
-			console.log("AJAX ERROR"); // NB not called for jsonp, which we are using
-			console.log([xhr, text, error]);
-		}
-	});
-
 	init_elements = function() {
-		// TODO set the subreddit value to the last value the user had set it to
 		init_subreddit_input();
-		fetch_new_subreddit();
 	}
 
 	init_subreddit_input = function() {
-		$subreddit_input.val(settings.last_visited_subreddit);
-		$subreddit_input.keyup(wait_to_fetch_new_subreddit);
+		var last_visited_subreddit = settings.last_visited_subreddit();
+		$subreddit_input.val(last_visited_subreddit);
+		//$subreddit_input.keyup(wait_to_fetch_new_subreddit);
 	}
 
 	wait_to_fetch_new_subreddit = function(e) {
@@ -79,7 +95,7 @@ var Reddit = function() {
 
 	handle_new_subreddit_data = function(data) {
 		clearTimeout(subreddit_jsonp_wait_event);
-		settings.last_visited_subreddit = $subreddit_input.val();
+		settings.last_visited_subreddit($subreddit_input.val());
 		set_subreddit_input_status("success");
 		display_reddits(data);
 	}
@@ -110,13 +126,123 @@ var Reddit = function() {
 	}
 
 	init_elements();
+	//fetch_new_subreddit();
 }
 
-var reddit = new Reddit();
+var reddit_ui = new Reddit_UI();
+
+
+// Handles user authentication and page personalisation,
+// as well as up and downvotes.
+var User = function() {
+	/*
+	 * in charge of
+	 * getting the user auth_token
+	 * saving and fetching the user auth_token and login status
+	 * displaying login and default subreddits, or the user subreddits and logout
+	 */
+	var default_subreddits = ["All", "New", "Random", "funny", "pics", "WorldNews", "politics", "gaming", "WTF"];
+
+	initialise_login = function() {
+		var existing_user = settings.user();
+		var existing_modhash = settings.modhash();
+
+		if (existing_user != "" && existing_modhash != "") {
+			hide_login();
+			get_user_subreddits();
+		}
+		else {
+			show_login();
+			show_default_subreddits();
+		}
+
+		$("#login_button").click(function() {
+			set_iframe_content();
+			show_iframe();
+		});
+		$("#modhash_button").click(function() {
+			save_login_credentials();
+			hide_login();
+			get_user_subreddits();
+		});
+		$("#logout_button").click(function() {
+			perform_logout();
+			show_login();
+		});
+	}
+
+	set_iframe_content = function() {
+		var template = _.template($("#login_form").html());
+		var params = {
+			user: $("#user").val(),
+			password: $("#password").val(),
+			endscript: "</script>"
+		}
+		var content = template(params);
+		var form = "<html><body>" + content + "</body></html>";
+		var form64 = btoa(form);
+		var datauri = "data:text/html;base64," + form64;
+		$("#login_iframe").attr("src", datauri);
+	}
+
+	show_iframe = function() {
+		$("#login_stage_1").css("display", "none");
+		$("#login_stage_2").css("display", "block");
+	}
+
+	save_login_credentials = function() {
+		settings.user($("#user").val());
+		settings.modhash($("#modhash").val());
+	}
+
+	show_login = function() {
+		$("#login_form_container").css("display", "block");
+		$("#logout_form_container").css("display", "none");
+	}
+	
+	hide_login = function() {
+		$("#login_form_container").css("display", "none");
+		$("#logout_form_container").css("display", "block");
+		$("#currently_logged_in_username").text(settings.user());
+	}
+
+	get_user_subreddits = function() {
+		// Try to get user subreddits
+		// If error, show login and error and show_default_subreddits()
+		// show_user_subreddits()
+	}
+
+	show_default_subreddits = function() {
+	}
+
+	show_user_subreddits = function() {
+	}
+
+	perform_logout = function() {
+		settings.user("");
+		settings.modhash("");
+		// TODO some ajax (maybe in iframe) to log the user out
+	}
+
+	initialise_login();
+}
+
+var user = new User();
 
 
 
 
+
+
+
+// some utility functions
+
+$.ajaxSetup({
+	error: function(xhr, text, error) {
+		console.log("AJAX ERROR"); // NB not called for jsonp, which we are using
+		console.log([xhr, text, error]);
+	}
+});
 
 Number.prototype.toFuzzyTime = function() {
 	var s_gone = (new Date().getTime()/1000) - this;
---
+++ src/serve_watch_and_compile.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"github.com/howeyc/fsnotify"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+
+/* File watcher */
+
+func start_watcher() {
+	watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // Process events
+    go func() {
+        for {
+            select {
+            case ev := <-watcher.Event:
+				if !strings.Contains(ev.Name, ".kate-swp") {
+					if ev.IsModify() {
+						compile()
+					}
+				}
+            case err := <-watcher.Error:
+                fmt.Println("error:", err)
+            }
+        }
+    }()
+
+    err = watcher.Watch(".")
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func ReadFileAsString(filename string) string {
+	val_b, err := ioutil.ReadFile(filename)
+	if err != nil { panic(err) }
+	val := string(val_b)
+	return val
+}
+
+func compile() {
+	fmt.Println("Compiling")
+
+	html := ReadFileAsString("r.html")
+	stylecss := ReadFileAsString("style.css")
+    appjs := ReadFileAsString("app.js")
+	jqueryjs := ReadFileAsString("jquery.js")
+	underscorejs := ReadFileAsString("underscore.js")
+
+	const  Style = "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">"
+	newstyle := "<style type=\"text/css\">\n" + stylecss + "\n</style>"
+	styled_html := strings.Replace(html, Style, newstyle, 1)
+
+	const Jquery = "<script src=\"jquery.js\"></script>"
+	newjquery := "<script type=\"text/javascript\">\n" + jqueryjs + "\n</script>"
+	jquery_html := strings.Replace(styled_html, Jquery, newjquery, 1)
+
+	const Underscore = "<script src=\"underscore.js\"></script>"
+	newunderscore := "<script type=\"text/javascript\">\n" + underscorejs + "\n</script>"
+	underscore_html := strings.Replace(jquery_html, Underscore, newunderscore, 1)
+
+	const App = "<script src=\"app.js\"></script>"
+	newapp := "<script type=\"text/javascript\">\n" + appjs + "\n</script>"
+	app_html := strings.Replace(underscore_html, App, newapp, 1)
+	
+	b_html := []byte(app_html)
+	err := ioutil.WriteFile("../reddit.html", b_html, 0644)
+    if err != nil { panic(err) }
+}
+
+
+
+/* Web server */
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.URL.Path, "..") {
+		fmt.Fprint(w, "Invalid path")
+	} else {
+		http.ServeFile(w, r, "." + r.URL.Path)
+	}
+}
+
+func start_server() {
+	var h Hello
+	http.ListenAndServe("localhost:8000",h)
+}
+
+func main() {
+
+	start_watcher()
+	start_server()
+
+	// watcher.Close()
+}
